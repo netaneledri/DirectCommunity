e251206f2f8df739a098ce1a64fde4f72d715cd5
Netanel Edri <netanelweb@gmail.com>
2013-10-16 22:09:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * DirectCommunity
+ * Forums community web application built on top of Direct framework
+ * @version 1.0.0
+ * @author Netanel Edri <www.netaneledri.net>
+ * @link www.projects.directcode.net/community
+ * @license www.projects.directcode.net/community/license
+ */
+
+
+
+
+
+
+?>
