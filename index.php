ad8be1c0dee941160a6ea3c50e191ce4c7cec2d4
Netanel Edri <netanelweb@gmail.com>
2013-10-16 22:13:02 +0200

    index file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * DirectCommunity
+ * Forums community web application built on top of Direct framework
+ * @version 1.0.0
+ * @author Netanel Edri <www.netaneledri.net>
+ * @link www.projects.directcode.net/community
+ * @license www.projects.directcode.net/community/license
+ */
+
+
+
+?>
